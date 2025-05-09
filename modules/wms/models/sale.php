107e0f22fbeff285097db22b2bf23128fb82e345
pacpoom <pacpoom09@gmail.com>
2025-05-09 11:21:42 +0700

    .

--- modules/wms/models/sale.php
+++ modules/wms/models/sale.php
@@ -18,14 +18,19 @@ class model extends \Kotchasan\Model{
         
             if ($params['status'] == 0) {
                 $where[] = array('T1.status',0);
+
             } elseif ($params['status'] == 1){
-                $where[] = array('T1.status',1)
+                $where[] = array('T1.status',1);
+
             } elseif ($params['status'] == 2) {
                 $where[] = array('T1.status',2);
+
             } elseif ($params['status'] == 3) {
                 $where[] = array('T1.status',3);
+
             } elseif ($params['status'] == 4) {
                 $where[] = array('T1.status',4);
+                
             }
 
             if (!empty($params['so'])) {
